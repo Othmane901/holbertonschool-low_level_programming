979f04a8665196dc4a66d69c8c1b475092763571
Othmane901 <bengharbi12@gmail.com>
2022-09-29 06:21:55 -0700

    c

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main - Its main
- *
- * Return: Its 0
- */
+* main - Its main
+*
+* Return: Its 0
+*/
 int main(void)
 {
-  printf("with proper grammar, but the outcome is a piece of art,\n");
-  return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
