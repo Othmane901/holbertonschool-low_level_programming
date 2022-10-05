b17c4cb778c245cbf96d937137272a9052228045
Othmane901 <bengharbi12@gmail.com>
2022-10-05 09:19:31 -0700

    c

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,16 +1,17 @@
-#ifndef main_h
-#define main_h
 int _putchar(char);
 void print_alphabet(void);
 void print_alphabet_x10(void);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
+int _islower(int);
+int _isalpha(int);
+int print_sign(int);
 int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
+int get_first(int);
+int get_last(int);
 void times_table(void);
 int add(int, int);
-void print_to_98(int n);
-void print_times_table(int n);
-#endif
+void print_any_int(int);
+void print_to_98(int);
+void print_times_table(int);
+long get_first_l(long);
