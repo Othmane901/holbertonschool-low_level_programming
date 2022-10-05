78ee30c11790de7722a678b339008544495928af
Othmane901 <bengharbi12@gmail.com>
2022-10-05 09:24:03 -0700

    c

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -7,10 +7,10 @@
 
 int main(void)
 {
-chat*message = "_putchar";
+char*message = "_putchar";
 int i = 0;
 do{
-_ptchat(message[i++]);
+_putchar(message[i++]);
 } while (message[i] != '\0');
 _putchar('\n');
 return (0);
