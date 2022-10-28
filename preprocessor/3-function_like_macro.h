71bfe6006fb16b66a803c39efed89e1ffb58ad2d
Othmane901 <bengharbi12@gmail.com>
2022-10-27 21:50:53 -0700

    c

--- preprocessor/3-function_like_macro.h
+++ preprocessor/3-function_like_macro.h
@@ -1,4 +1,6 @@
-#ifndef FUNCTION_LIKE_MACRO
-#define FUNCTION_LIKE_MACRO
-#define ABS(x) (x * ((x < 0) * (-1) + (x > 0)))
-#endif 
+#ifndef _3_FUNCTION_LIKE_MACRO_H_
+#define _3_FUNCTION_LIKE_MACRO_H_
+
+#define ABS(x) ((x) < (0) ? (-x) : (x))
+
+#endif
